cc25f48736f0f02d35c11cc863deff3f5e19bc84
Octocat& <3537256lol@gmail.com>
2026-01-01 05:26:59 +0700

    create files for classes

---
+++ task2-design-pattern-and-logging/src/Variables.java
@@ -0,0 +1,19 @@
+import java.util.Map;
+
+public class Variables {
+    Map<String, Double> varTable;
+
+    /**
+     * Store the variable
+     * 
+     * @param name
+     * @param value
+     */
+    public void define(String name, double value) {
+        // TODO
+    }
+
+    public double getValue(String name) {
+        // TODO
+    }
+}

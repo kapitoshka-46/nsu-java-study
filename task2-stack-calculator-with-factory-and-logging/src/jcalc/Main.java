453cce4dda78b88d1e324146419f605348ccaab7
Octocat& <3537256lol@gmail.com>
2026-01-06 03:47:45 +0700

    add todo

--- task2-stack-calculator-with-factory-and-logging/src/jcalc/Main.java
+++ task2-stack-calculator-with-factory-and-logging/src/jcalc/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("----------------------------------------------------");
-        CalculatorApp app = new CalculatorApp(System.in, System.out);
+        CalculatorApp app = new CalculatorApp(System.in, System.out); // TODO: Let calculator parse args
         app.run();
 
     }
